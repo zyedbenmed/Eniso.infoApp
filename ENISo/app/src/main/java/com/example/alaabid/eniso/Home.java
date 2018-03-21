2e94c4b25b40837f11d3fefc20ebf02eb9761039
Ala Abid <abidala99@yahoo.fr>
2018-03-21 17:51:12 +0100

    changed API + time table + bug fixes

--- ENISo/app/src/main/java/com/example/alaabid/eniso/Home.java
+++ ENISo/app/src/main/java/com/example/alaabid/eniso/Home.java
@@ -1,7 +1,10 @@
 package com.example.alaabid.eniso;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Home extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Button bt_timetable = findViewById(R.id.bt_timetable);
+        bt_timetable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),Timetable.class);
+                startActivity(i);
+            }
+        });
     }
 }
